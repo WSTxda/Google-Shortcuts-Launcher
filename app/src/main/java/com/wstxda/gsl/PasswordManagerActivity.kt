d6f7334709d1fafadb0e79c9a9b38aa933d1522c
WSTxda <wstxda@gmail.com>
2023-04-04 19:13:03 -0300

    Cleaning imports

--- app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
+++ app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
@@ -4,8 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 
 class PasswordManagerActivity : Activity() {
 
