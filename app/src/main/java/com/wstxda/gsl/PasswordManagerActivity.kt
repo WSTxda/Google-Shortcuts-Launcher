e1ae11f9eca6f1bb96df890fd4c6e673a4a6d8c0
WSTxda <wstxda@gmail.com>
2023-04-04 19:06:39 -0300

    Some changes

--- app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
+++ app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
@@ -8,8 +8,6 @@ import android.widget.Button
 import android.widget.TextView
 
 class PasswordManagerActivity : Activity() {
-    private val buttonOpenlink: Button? = null
-    private val textOpenlink: TextView? = null
 
     // android.app.Activity
     override fun onCreate(bundle: Bundle?) {
