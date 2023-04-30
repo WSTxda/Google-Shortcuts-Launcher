1418b3a471446c1855b0f04f2bab7e189e8f5e5e
WSTxda <wstxda@gmail.com>
2023-04-30 13:03:07 -0300

    Add game folder shortcurt

---
+++ app/src/main/java/com/wstxda/gsl/GameActivity.kt
@@ -0,0 +1,28 @@
+package com.wstxda.gsl
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Intent
+import android.widget.Toast
+
+@SuppressLint("Registered")
+class GameActivity : Activity() {
+    // android.app.Activity
+    public override fun onResume() {
+        super.onResume()
+        try {
+            val intent = Intent("android.intent.action.MAIN")
+            intent.addCategory("android.intent.category.LAUNCHER")
+            intent.component = ComponentName(
+                "com.google.android.play.games",
+                "com.google.android.apps.play.games.features.gamefolder.GameFolderTrampolineActivity"
+            )
+            startActivity(intent)
+            finish()
+        } catch (unused: Exception) {
+            Toast.makeText(applicationContext, R.string.activity_not_found_games, Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+}
