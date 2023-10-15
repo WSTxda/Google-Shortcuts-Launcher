4d70648e03d30d0dbc6b6f1a1dadcff9f27b7a01
WSTxda <wstxda@gmail.com>
2023-10-15 13:47:04 -0300

    Check if the browser app is available

--- app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
+++ app/src/main/java/com/wstxda/gsl/PasswordManagerActivity.kt
@@ -4,14 +4,23 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 
 class PasswordManagerActivity : Activity() {
     // android.app.Activity
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
-        val intent = Intent("android.intent.action.VIEW")
-        intent.data = Uri.parse("https://passwords.google.com")
-        startActivity(intent)
 
+        val url = getString(R.string.password_url)
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            val message = getString(R.string.browser_not_found)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
