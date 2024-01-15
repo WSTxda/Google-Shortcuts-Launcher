12f818ada8c28200b2889c79abffab9c1a5a4b0a
WSTxda <wstxda@gmail.com>
2024-01-15 20:03:43 -0300

    Open Game Turbo if available

--- app/src/main/java/com/wstxda/gsl/GameActivity.kt
+++ app/src/main/java/com/wstxda/gsl/GameActivity.kt
@@ -8,11 +8,21 @@ import android.widget.Toast
 class GameActivity : Activity() {
     override fun onResume() {
         super.onResume()
-        try {
-            val packageName = "com.google.android.play.games"
-            val className =
-                "com.google.android.apps.play.games.features.gamefolder.GameFolderTrampolineActivity"
 
+        // Try opening Game Turbo
+        if (!tryOpenActivity(
+                "com.miui.securitycenter", "com.miui.gamebooster.ui.GameBoosterMainActivity"
+            )
+        ) {
+            // If unsuccessful, open Google Play Games
+            openPlayGames()
+        }
+
+        finish()
+    }
+
+    private fun tryOpenActivity(packageName: String, className: String): Boolean {
+        try {
             val intent = Intent().apply {
                 action = Intent.ACTION_MAIN
                 addCategory(Intent.CATEGORY_LAUNCHER)
@@ -21,14 +31,35 @@ class GameActivity : Activity() {
 
             if (intent.component != null) {
                 startActivity(intent)
+                return true
+            }
+        } catch (e: Exception) {
+            // Handle any exceptions
+        }
+
+        return false
+    }
+
+    private fun openPlayGames() {
+        try {
+            val playGamesPackageName = "com.google.android.play.games"
+            val playGamesClassName =
+                "com.google.android.apps.play.games.features.gamefolder.GameFolderTrampolineActivity"
+
+            val playGamesIntent = Intent().apply {
+                action = Intent.ACTION_MAIN
+                addCategory(Intent.CATEGORY_LAUNCHER)
+                component = ComponentName(playGamesPackageName, playGamesClassName)
+            }
+
+            if (playGamesIntent.component != null) {
+                startActivity(playGamesIntent)
             } else {
                 showPlayGamesNotFoundMessage()
             }
         } catch (e: Exception) {
             showPlayGamesNotFoundMessage()
         }
-
-        finish()
     }
 
     private fun showPlayGamesNotFoundMessage() {
@@ -37,5 +68,4 @@ class GameActivity : Activity() {
         ).show()
     }
 }
-
 
