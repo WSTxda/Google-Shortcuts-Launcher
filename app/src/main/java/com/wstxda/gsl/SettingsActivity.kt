e977a14478dfba66627ab7297669fc5ccb7e1dac
WSTxda <wstxda@gmail.com>
2024-03-07 16:32:40 -0300

    Add preference links and version name for preferences

--- app/src/main/java/com/wstxda/gsl/SettingsActivity.kt
+++ app/src/main/java/com/wstxda/gsl/SettingsActivity.kt
@@ -1,9 +1,13 @@
 package com.wstxda.gsl
 
 import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 
@@ -26,6 +30,10 @@ class SettingsActivity : AppCompatActivity() {
             setupPreference("game_activity", GameActivity::class.java)
             setupPreference("music_search_activity", MusicSearchActivity::class.java)
             setupPreference("settings_activity", SettingsActivity::class.java)
+            developerLink("developer")
+            githubRepository("github_repository")
+            findPreference<Preference>("version")?.summary = getVersionName()
+
         }
 
         private fun setupPreference(key: String, activityClass: Class<*>) {
@@ -47,6 +55,36 @@ class SettingsActivity : AppCompatActivity() {
             packageManager.setComponentEnabledSetting(
                 componentName, newState, PackageManager.DONT_KILL_APP
             )
+        }
+
+        private fun getVersionName(): String {
+            return try {
+                val packageInfo: PackageInfo =
+                    requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
+                packageInfo.versionName
+            } catch (e: PackageManager.NameNotFoundException) {
+                "N/A"
+            }
+        }
+
+        private fun developerLink(key: String) {
+            val preference = findPreference<Preference>(key)
+            preference?.setOnPreferenceClickListener {
+                val url = "https://github.com/WSTxda"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+                true
+            }
+        }
+
+        private fun githubRepository(key: String) {
+            val preference = findPreference<Preference>(key)
+            preference?.setOnPreferenceClickListener {
+                val url = "https://github.com/WSTxda/Google-Shortcuts-Launcher"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+                true
+            }
         }
     }
 }
