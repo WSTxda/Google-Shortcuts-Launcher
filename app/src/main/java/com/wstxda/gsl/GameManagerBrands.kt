8cf944cd5b26899ba3da0935a64b3b1d7e032f67
WSTxda <wstxda@gmail.com>
2024-08-26 15:49:11 -0300

    Improvement GameManagerBrands list

--- app/src/main/java/com/wstxda/gsl/GameManagerBrands.kt
+++ app/src/main/java/com/wstxda/gsl/GameManagerBrands.kt
@@ -8,65 +8,83 @@ import android.content.Intent
 class GameManagerBrands(private val context: Context) {
 
     private val gameManagerIntents = listOf(
-        // Xiaomi (Global/China)
-        "com.miui.securitycenter" to "com.miui.gamebooster.ui.GameBoosterMainActivity",
-        "com.miui.securitycenter" to "com.miui.gamebooster.mygame.MyGameActivity",
 
-        // Samsung (Global/Europe/Asia)
-        "com.samsung.android.game.gametools" to "com.samsung.android.game.gametools.main.MainActivity",
-        "com.samsung.android.game.gamehome" to "com.samsung.android.game.gamehome.MainActivity",
-
-        // Oppo (Global/China)
-        "com.oppo.gamecenter" to "com.oppo.gamecenter.GameCenterActivity",
-        "com.oppo.gamespace" to "com.oppo.gamespace.ui.GameSpaceMainActivity",
-
-        // Vivo (Global/China)
-        "com.vivo.game" to "com.vivo.game.GameActivity",
-        "com.vivo.gamespace" to "com.vivo.gamespace.ui.GameSpaceMainActivity",
-
-        // ASUS (Global)
+        // ASUS
         "com.asus.gamecenter" to "com.asus.gamecenter.GameCenterActivity",
+        "com.asus.game" to "com.asus.game.ui.GameLauncherActivity",
 
-        // Huawei (Global/China)
-        "com.huawei.gameassistant" to "com.huawei.gameassistant.ui.MainActivity",
-        "com.huawei.gameassistant" to "com.huawei.gameassistant.ui.GameSpaceMainActivity",
-
-        // OnePlus (Global/China)
-        "com.oneplus.game" to "com.oneplus.gamebox.GameBoxLauncherActivity",
-        "com.oneplus.gamespace" to "com.oneplus.gamespace.ui.GameSpaceMainActivity",
-
-        // Realme (Global/China)
-        "com.realme.game" to "com.realme.game.GameSpaceMainActivity",
-        "com.realme.gamespace" to "com.realme.gamespace.ui.GameSpaceMainActivity",
-
-        // Lenovo (Global/China)
-        "com.lenovo.gamezone" to "com.lenovo.gamezone.ui.GameZoneMainActivity",
-        "com.lenovo.gamespace" to "com.lenovo.gamespace.ui.GameSpaceMainActivity",
-
-        // ASUS ROG (Global)
+        // ASUS ROG
         "com.rog.game" to "com.rog.game.launcher.MainActivity",
+        "com.rog.game" to "com.rog.game.launcher.GameLauncherActivity",
 
-        // Black Shark (Global/China)
+        // Black Shark
         "com.blackshark.gamespace" to "com.blackshark.gamespace.ui.MainActivity",
         "com.blackshark.gamespace" to "com.blackshark.gamespace.ui.GameSpaceMainActivity",
+        "com.blackshark.gamespace" to "com.blackshark.gamespace.ui.GameCenterActivity",
 
-        // Nubia Red Magic (Global/China)
-        "com.redmagic.gamecenter" to "com.redmagic.gamecenter.GameCenterMainActivity",
-        "com.redmagic.gamecenter" to "com.redmagic.gamecenter.GameSpaceMainActivity",
+        // Huawei
+        "com.huawei.gameassistant" to "com.huawei.gameassistant.ui.MainActivity",
+        "com.huawei.gameassistant" to "com.huawei.gameassistant.ui.GameSpaceMainActivity",
+        "com.huawei.gameassistant" to "com.huawei.gameassistant.ui.GameManagerActivity",
 
-        // Sony Xperia (Global)
-        "com.sonymobile.gameenhancer" to "com.sonymobile.gameenhancer.GameEnhancerMainActivity",
+        // Lenovo
+        "com.lenovo.gamezone" to "com.lenovo.gamezone.ui.GameZoneMainActivity",
+        "com.lenovo.gamespace" to "com.lenovo.gamespace.ui.GameSpaceMainActivity",
+        "com.lenovo.gamezone" to "com.lenovo.gamezone.ui.GameManagerActivity",
 
-        // Meizu (China)
+        // Meizu
         "com.meizu.flyme.gamecenter" to "com.meizu.flyme.gamecenter.GameCenterMainActivity",
         "com.meizu.gamespace" to "com.meizu.gamespace.ui.GameSpaceMainActivity",
+        "com.meizu.flyme.gamecenter" to "com.meizu.flyme.gamecenter.GameManagerActivity",
 
-        // ZTE (Global/China)
-        "com.zte.gamespace" to "com.zte.gamespace.ui.GameSpaceMainActivity",
-
-        // Motorola (Global)
+        // Motorola
         "com.motorola.gametime" to "com.motorola.gametime.ui.GameTimeMainActivity",
-        "com.motorola.gametime" to "com.motorola.gametime.GameTimeMainActivity"
+        "com.motorola.gametime" to "com.motorola.gametime.GameTimeMainActivity",
+        "com.motorola.gametime" to "com.motorola.gametime.GameManagerActivity",
+
+        // Nubia Red Magic
+        "com.redmagic.gamecenter" to "com.redmagic.gamecenter.GameCenterMainActivity",
+        "com.redmagic.gamecenter" to "com.redmagic.gamecenter.GameSpaceMainActivity",
+        "com.redmagic.gamespace" to "com.redmagic.gamespace.ui.GameManagerActivity",
+
+        // OnePlus
+        "com.oneplus.game" to "com.oneplus.gamebox.GameBoxLauncherActivity",
+        "com.oneplus.gamespace" to "com.oneplus.gamespace.ui.GameSpaceMainActivity",
+        "com.oneplus.game" to "com.oneplus.gamebox.GameManagerActivity",
+
+        // Oppo
+        "com.oppo.gamecenter" to "com.oppo.gamecenter.GameCenterActivity",
+        "com.oppo.gamespace" to "com.oppo.gamespace.ui.GameSpaceMainActivity",
+        "com.oppo.gamecenter" to "com.oppo.gamecenter.GameManagerActivity",
+
+        // Realme
+        "com.realme.game" to "com.realme.game.GameSpaceMainActivity",
+        "com.realme.gamespace" to "com.realme.gamespace.ui.GameSpaceMainActivity",
+        "com.realme.game" to "com.realme.game.GameManagerActivity",
+
+        // Samsung
+        "com.samsung.android.game.gamehome" to "com.samsung.android.game.gamehome.MainActivity",
+        "com.samsung.android.game.gamehome" to "com.samsung.android.game.gamehome.app.MainActivity",
+        "com.samsung.android.game.gametools" to "com.samsung.android.game.gametools.main.MainActivity",
+        "com.samsung.android.game.gametools" to "com.samsung.android.game.gametools.GameManagerActivity",
+
+        // Sony Xperia
+        "com.sonymobile.gameenhancer" to "com.sonymobile.gameenhancer.GameEnhancerMainActivity",
+        "com.sonymobile.gameenhancer" to "com.sonymobile.gameenhancer.GameManagerActivity",
+
+        // Vivo
+        "com.vivo.game" to "com.vivo.game.GameActivity",
+        "com.vivo.gamespace" to "com.vivo.gamespace.ui.GameSpaceMainActivity",
+        "com.vivo.game" to "com.vivo.game.GameManagerActivity",
+
+        // Xiaomi
+        "com.miui.securitycenter" to "com.miui.gamebooster.mygame.MyGameActivity",
+        "com.miui.securitycenter" to "com.miui.gamebooster.ui.GameBoosterMainActivity",
+        "com.miui.securitycenter" to "com.miui.gamebooster.ui.GameManagerActivity",
+
+        // ZTE
+        "com.zte.gamespace" to "com.zte.gamespace.ui.GameSpaceMainActivity",
+        "com.zte.gamespace" to "com.zte.gamespace.ui.GameManagerActivity"
     )
 
     fun launchGameManager(): Boolean {
