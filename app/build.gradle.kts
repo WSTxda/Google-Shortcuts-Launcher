61895683be6587c680b2b4b98964e1f203b27bed
WSTxda <wstxda@gmail.com>
2024-09-16 19:45:56 -0300

    Remove test implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,4 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
