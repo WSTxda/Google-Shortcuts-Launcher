f12178032f35bc2cc0a35737af7c98d5dc1dcbbb
WSTxda <wstxda@gmail.com>
2024-10-30 07:13:11 -0300

    Update gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
 }
