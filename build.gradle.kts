06f9ea880859014d453d8a1ab8432aaa5c78712e
WSTxda <wstxda@gmail.com>
2024-11-11 12:57:41 -0300

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
