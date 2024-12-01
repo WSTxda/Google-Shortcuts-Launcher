32cbf87d6805757651f6c600ca2ecb264bc8e83a
WSTxda <wstxda@gmail.com>
2024-12-01 13:25:37 -0300

    Added back setup assistant bottom sheet in games

--- app/src/main/java/com/wstxda/gsl/shortcut/GamesActivity.kt
+++ app/src/main/java/com/wstxda/gsl/shortcut/GamesActivity.kt
@@ -1,8 +1,11 @@
 package com.wstxda.gsl.shortcut
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.wstxda.gsl.R
 import com.wstxda.gsl.utils.GamesBrandsPackages
 import com.wstxda.gsl.utils.IntentHelper
@@ -11,16 +14,28 @@ class GamesActivity : AppCompatActivity() {
 
     companion object {
         private const val GAME_MANAGER_PREF_KEY = "device_game_manager"
+        private const val SHEET_SHOWN_PREF_KEY = "sheetShown"
+    }
+
+    private val sharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        launchGames()
+        checkAndHandleBottomSheetDialog()
+    }
+
+    private fun checkAndHandleBottomSheetDialog() {
+        if (!sharedPreferences.getBoolean(SHEET_SHOWN_PREF_KEY, false)) {
+            showAssistantBottomSheet()
+        } else {
+            launchGames()
+        }
     }
 
     private fun launchGames() {
-        val useGameManager = PreferenceManager.getDefaultSharedPreferences(this)
-            .getBoolean(GAME_MANAGER_PREF_KEY, false)
+        val useGameManager = sharedPreferences.getBoolean(GAME_MANAGER_PREF_KEY, false)
 
         if (useGameManager) {
             if (!launchBrandGamesManager()) {
@@ -43,7 +58,45 @@ class GamesActivity : AppCompatActivity() {
 
     private fun launchGooglePlayGamesFolder(): Boolean {
         return IntentHelper.tryStartActivity(
-            this, "com.google.android.play.games", "com.google.android.apps.play.games.features.gamefolder.GameFolderTrampolineActivity"
+            this,
+            "com.google.android.play.games",
+            "com.google.android.apps.play.games.features.gamefolder.GameFolderTrampolineActivity"
         )
+    }
+
+    private fun showAssistantBottomSheet() {
+        val bottomSheetView = layoutInflater.inflate(
+            R.layout.bottom_sheet_assistant, findViewById(android.R.id.content), false
+        ) ?: return
+
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        bottomSheetView.findViewById<Button>(R.id.setup_button).setOnClickListener {
+            openAssistantSettings()
+            markSheetAsShown()
+            bottomSheetDialog.dismiss()
+            finish()
+        }
+
+        bottomSheetView.findViewById<Button>(R.id.cancel_button).setOnClickListener {
+            markSheetAsShown()
+            bottomSheetDialog.dismiss()
+            launchGames()
+        }
+
+        bottomSheetDialog.setOnCancelListener {
+            finish()
+        }
+
+        bottomSheetDialog.show()
+    }
+
+    private fun openAssistantSettings() {
+        startActivity(Intent(android.provider.Settings.ACTION_VOICE_INPUT_SETTINGS))
+    }
+
+    private fun markSheetAsShown() {
+        sharedPreferences.edit().putBoolean(SHEET_SHOWN_PREF_KEY, true).apply()
     }
 }
