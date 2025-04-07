01f31efcd9a7bad7c0c35964fcc16a26d868d370
WSTxda <wstxda@gmail.com>
2025-04-07 15:15:44 -0300

    Improvement `DigitalAssistantSetupDialog` UI

--- app/src/main/java/com/wstxda/gsl/ui/DigitalAssistantSetupDialog.kt
+++ app/src/main/java/com/wstxda/gsl/ui/DigitalAssistantSetupDialog.kt
@@ -1,11 +1,11 @@
 package com.wstxda.gsl.ui
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Button
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -33,17 +33,21 @@ class DigitalAssistantSetupDialog : DialogFragment() {
         val digitalAssistantPreference = DigitalAssistantPreference(
             parentFragment as PreferenceFragmentCompat
         )
+        val dialogView = layoutInflater.inflate(R.layout.assistant_setup_dialog, null)
+        val dialog = MaterialAlertDialogBuilder(
+            requireContext(), R.style.Theme_Component_AlertDialog_Assistant
+        ).setView(dialogView).setCancelable(false).create()
 
-        return MaterialAlertDialogBuilder(
-            requireContext(), R.style.Theme_Component_AlertDialog_Assistant)
-            .setIcon(R.drawable.ic_assistant_setup_open)
-            .setTitle(R.string.digital_assistant_setup_title)
-            .setMessage(R.string.digital_assistant_setup_message)
-            .setPositiveButton(R.string.setup_now_button) { _: DialogInterface, _: Int ->
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-                    digitalAssistantPreference.setDigitalAssistSetupStatus(requireContext(), true)
-                }
-                launcher.launch(Intent(Settings.ACTION_VOICE_INPUT_SETTINGS))
-            }.setCancelable(false).create()
+        dialogView.findViewById<Button>(R.id.positive_button).setOnClickListener {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                digitalAssistantPreference.setDigitalAssistSetupStatus(requireContext(), true)
+            }
+            launcher.launch(Intent(Settings.ACTION_VOICE_INPUT_SETTINGS))
+            dialog.dismiss()
+        }
+        dialogView.findViewById<Button>(R.id.negative_button).setOnClickListener {
+            dialog.dismiss()
+        }
+        return dialog
     }
 }
