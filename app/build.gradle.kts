0f8ad398571ed07feb73b36bce4786bbb5290b01
WSTxda <wstxda@gmail.com>
2025-04-10 21:24:19 -0300

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wstxda.gsl"
         minSdk = 24
         targetSdk = 35
-        versionCode = 520
-        versionName = "5.2"
+        versionCode = 530
+        versionName = "5.3"
     }
 
     buildTypes {
