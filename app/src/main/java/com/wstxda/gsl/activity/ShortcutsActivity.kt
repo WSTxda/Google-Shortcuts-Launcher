9ab5aceb1acce2087e703f7e7aadcc80eba11c74
WSTxda <wstxda@gmail.com>
2025-05-07 01:11:51 -0300

    Fix bars color flicker when launch landscape shortcuts

--- app/src/main/java/com/wstxda/gsl/activity/ShortcutsActivity.kt
+++ app/src/main/java/com/wstxda/gsl/activity/ShortcutsActivity.kt
@@ -1,11 +1,15 @@
 package com.wstxda.gsl.activity
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 abstract class ShortcutsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         onCreateInternal()
         finish()
     }
