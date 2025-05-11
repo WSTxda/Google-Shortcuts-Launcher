42c14d9b9cce406025c6e420022d43a6863acd3c
WSTxda <wstxda@gmail.com>
2025-05-11 06:07:28 -0300

    Added more browsers to launch incognito shortcut

--- app/src/main/java/com/wstxda/gsl/shortcuts/IncognitoShortcut.kt
+++ app/src/main/java/com/wstxda/gsl/shortcuts/IncognitoShortcut.kt
@@ -1,19 +1,20 @@
 package com.wstxda.gsl.shortcuts
 
-import android.content.ComponentName
 import android.content.Intent
 import com.wstxda.gsl.R
 import com.wstxda.gsl.activity.ShortcutsActivity
 import com.wstxda.gsl.logic.launchShortcuts
+import com.wstxda.gsl.utils.IncognitoBrowsersPackages
 
 class IncognitoShortcut : ShortcutsActivity() {
     override fun onCreateInternal() {
-        launchShortcuts(listOf(createIncognitoIntent()), R.string.chrome_not_found)
-    }
-
-    private fun createIncognitoIntent() = Intent().apply {
-        component = ComponentName(
-            "com.android.chrome", "org.chromium.chrome.browser.incognito.IncognitoTabLauncher"
-        )
+        val intents = IncognitoBrowsersPackages.incognitoLauncherIntents.map { (pkg, cls) ->
+            Intent().apply {
+                setClassName(pkg, cls)
+                action = Intent.ACTION_MAIN
+                addCategory(Intent.CATEGORY_LAUNCHER)
+            }
+        }
+        launchShortcuts(intents, R.string.chrome_not_found)
     }
 }
---
+++ app/src/main/java/com/wstxda/gsl/utils/IncognitoBrowsersPackages.kt
@@ -0,0 +1,14 @@
+package com.wstxda.gsl.utils
+
+object IncognitoBrowsersPackages {
+    val incognitoLauncherIntents = listOf(
+        // Google Chrome
+        "com.android.chrome" to "org.chromium.chrome.browser.incognito.IncognitoTabLauncher",
+        "com.chrome.canary" to "org.chromium.chrome.browser.incognito.IncognitoTabLauncher",
+        "com.chrome.beta" to "org.chromium.chrome.browser.incognito.IncognitoTabLauncher",
+        "com.chrome.dev" to "org.chromium.chrome.browser.incognito.IncognitoTabLauncher",
+
+        // Brave Browser
+        "com.brave.browser" to "org.chromium.chrome.browser.incognito.IncognitoTabLauncher"
+    )
+}
