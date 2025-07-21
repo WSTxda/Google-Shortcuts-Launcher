3b7cdecca3690dedff4e9f1c67867caae8ed4c65
WSTxda <wstxda@gmail.com>
2025-07-20 22:04:22 -0300

    Small changes

--- app/src/main/java/com/wstxda/gsl/services/DigitalAssistantService.kt
+++ app/src/main/java/com/wstxda/gsl/services/DigitalAssistantService.kt
@@ -3,7 +3,7 @@ package com.wstxda.gsl.services
 import com.wstxda.gsl.activity.ShortcutsActivity
 import com.wstxda.gsl.utils.DigitalAssistantShortcuts
 
-class AssistantFallbackService : ShortcutsActivity() {
+class DigitalAssistantService : ShortcutsActivity() {
     override fun onCreateInternal() {
         DigitalAssistantShortcuts.launchSelectedShortcut(this)
     }
