d2c575d5a0366fdebf6f9a23c7b800ccbf7f41d3
WSTxda <wstxda@gmail.com>
2025-10-01 20:59:12 -0300

    Improve music search tile

--- app/src/main/java/com/wstxda/gsl/services/MusicSearchTileService.kt
+++ app/src/main/java/com/wstxda/gsl/services/MusicSearchTileService.kt
@@ -2,7 +2,9 @@ package com.wstxda.gsl.services
 
 import android.annotation.SuppressLint
 import android.app.PendingIntent
+import android.graphics.drawable.Icon
 import android.os.Build
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import com.wstxda.gsl.R
 import com.wstxda.gsl.logic.launchShortcuts
@@ -10,6 +12,16 @@ import com.wstxda.gsl.logic.showToast
 import com.wstxda.gsl.utils.IntentFactory
 
 class MusicSearchTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+        updateTile()
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        updateTile()
+    }
 
     @SuppressLint("StartActivityAndCollapseDeprecated")
     override fun onClick() {
@@ -26,6 +38,18 @@ class MusicSearchTileService : TileService() {
             } else {
                 @Suppress("DEPRECATION") startActivityAndCollapse(intent)
             }
+        }
+    }
+
+    private fun updateTile() {
+        qsTile?.apply {
+            label = getString(R.string.music_search_label)
+            icon = Icon.createWithResource(this@MusicSearchTileService, R.drawable.ic_music_search_tile)
+            state = Tile.STATE_INACTIVE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                subtitle = getString(R.string.music_search_subtitle)
+            }
+            updateTile()
         }
     }
 }
